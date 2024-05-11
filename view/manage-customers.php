d3fd6cc68024b3a4af03f192118fe0944d0ac582
Sowad-Hossain <sowadhn@gmail.com>
2024-05-12 02:17:02 +0600

    man

--- view/manage-customers.php
+++ view/manage-customers.php
@@ -8,8 +8,6 @@
 <body>
 <?php require_once('navbar.php') ?>
 <?php require_once('side-panel.php') ?>
-<!-- fahim: customer table -->
-<!-- ratul: fetch kor -->
     <table class="customers-table" id="customers-table">
         <tr>
             <td colspan="6">
@@ -26,7 +24,6 @@
             <td> 1 </td>
             <td> Rianul Amin </td>
             <td> ppsppspsspss </td>
-            <!-- ratul: eki file shobar leiga oi file gulay id pass koira dis edit view ban -->
             <td> <a href="view-profile.php">View Details</a> </td>
             <td> <a href="edit-profile-info.php">Update Information</a> </td>
             <td> <a href="">Ban Customer</a> </td>
