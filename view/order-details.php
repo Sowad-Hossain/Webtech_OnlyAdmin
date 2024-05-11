e19fa30725d49426498ca3d81329315ece919d16
Sowad-Hossain <sowadhn@gmail.com>
2024-05-12 02:21:39 +0600

    df

--- view/order-details.php
+++ view/order-details.php
@@ -8,16 +8,14 @@
 <body>
 <?php require_once('navbar.php') ?>
 <?php require_once('side-panel.php') ?>
-    <!-- fahim: order details table -->
-    <!-- ratul: info fetch kor -->
     <table class="order-details-table" id="order-details-table">
         <tr>
             <td>
-                Username: 
-                Order ID: 
-                Order Date: 
+                Username:
+                Order ID:
+                Order Date:
                 Total Amount:
-                Order Status: 
+                Order Status:
             </td>
         </tr>
     </table>
